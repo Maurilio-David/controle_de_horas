d9ff3651a6306e986bd1b4aeca727fb937e0d76f
Maurilio-David <65241505+Maurilio-David@users.noreply.github.com>
2024-05-09 15:19:50 -0300

    feat: configure gitignore. configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:controle_horas/screens/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
